5d2e8fb41fcf3445dc2baf38da1c9f396a6b6a21
Max Röhrl <max.roehrl11@gmail.com>
2018-04-02 17:56:24 +0200

    Update mail icon and add contact icon class

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Scroll to the id if a navigation link is clicked.
     $('.navbar > a').on('click', function (event) {
         var hash = this.hash;
         if (hash !== "") {
