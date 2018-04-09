65e8beec9c894db6f7e0e3a78c764d96e4a94fea
Max Röhrl <max.roehrl11@gmail.com>
2018-04-09 16:54:03 +0200

    Update dist

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,2 +1,2 @@
-$(document).ready(function(){$(".navbar > a").on("click",function(t){var a=this.hash;""!==a&&(t.preventDefault(),$("html, body").animate({scrollTop:$(a).offset().top},800,function(){window.location.hash=a}))}),$("main a[href ^= 'http']").attr("target","_blank")});
+$(document).ready(function(){$(".navbar > a").on("click",function(t){var a=this.hash;""!==a&&(t.preventDefault(),$("html, body").animate({scrollTop:$(a).offset().top},800,function(){window.location.hash=a}))}),$("main a[href ^= 'http']").attr("target","_blank");var t=$("#overlay");$("img").click(function(){t.fadeTo(600,1).css({backgroundImage:"url("+this.src+")"})}),t.click(function(){$(this).stop().fadeTo(600,0,function(){$(this).hide()})})});
 //# sourceMappingURL=script.js.map
