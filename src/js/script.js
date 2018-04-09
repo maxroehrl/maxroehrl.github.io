ed86ab3bd7c224fba1d6c68e1a22141e6435c6f0
Max Röhrl <max.roehrl11@gmail.com>
2018-04-09 12:58:43 +0200

    Add image overlay

--- src/js/script.js
+++ src/js/script.js
@@ -12,4 +12,18 @@ $(document).ready(function () {
         }
     });
     $("main a[href ^= 'http']").attr("target", "_blank");
+
+    // Image overlay
+    var overlay = $('#overlay');
+
+    $('img').click(function () {
+        overlay.fadeTo(600, 1).css({
+            backgroundImage: "url(" + this.src + ")"
+        });
+    });
+    overlay.click(function () {
+        $(this).stop().fadeTo(600, 0, function () {
+            $(this).hide();
+        });
+    });
 });
